711e8ebd12c59caa3f29940337c59c1adf47c8da
marciogoda <marcio.goda@iongroup.com>
2025-05-07 15:32:23 +0100

    set required terraform version

--- version.tf
+++ version.tf
@@ -1,8 +1,9 @@
 terraform {
-  required_providers {
-    template = {
+    required_providers {
+      template = {
         source  = "hashicorp/template"
         version = "~> 2.2"
+      }
     }
-  }
+    required_version = ">= 1.1"
 }
